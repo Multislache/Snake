b2738b8fff8ca9954116a33935c6682e7166278f
Multislache <118292542+Multislache@users.noreply.github.com>
2022-12-09 15:20:58 +0100

    Pommev1

--- Pomme.py
+++ Pomme.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from moicrobit import *
-import File as f1
+#from microbit import *
+import Cellule as fl
 import Serpent as sp
 import random
 class Pomme:
     def __ini__(self):
-        self.coordonnées = (None,None)
-    def aleatoire(self):
-        list1 = [0, 1 ,2 ,3 ,4]
-        self.coordonnées = random.choice(list1)
+        self.coordonnees = (random.randint(0,4),random.randint(0,4))
+        while sp.corps.appartient(self.coordonnees):
+            self.coordonnees = (random.randint(0,4),random.randint(0,4))
+        
+#class Amanger:
+#    def __init__(self):
+#        self.amanger = 
+        
+    
