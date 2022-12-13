43a3ade270ca5c58b12fe7e1a4314e14e931e095
Aliinnk <118292890+Aliinnk@users.noreply.github.com>
2022-12-13 17:20:57 +0100

    Add files via upload

--- SNAKE.py
+++ SNAKE.py
@@ -1,31 +1,41 @@
-from microbit import *
 import File as fl
-class Serpent():
+import microbit as micro
+class Serpent:
     def __init__(self):
-        self.corps = f1.File()
+        self.corps = fl.File()
+        self.corps.enfile((0,0))
+        self.corps.enfile((0,1))
         self.direction = (1,0)
-    """
+
     def get_tete(self):
-        return
+        return self.corps.dernier
     def get_queue(self):
-        return
-    def set_tete(self):
-        return
-    def set_queue(self):
-        return
-    """
+        return self.corps.premier
+    def set_tete(self, x):
+        self.corps.dernier = x
+    def set_queue(self,x ):
+        self.corps.premier = x
+
     def avance(self):
         self.corps.fl.enfile(1)
         self.corps.fl.defile()
 
     def tourne(self, ancienx, ancieny):
-        differencex = accelerometer.get_x() - ancienx
-        differencey = accelerometer.get_y() - ancieny
+        differencex = micro.accelerometer.get_x() - ancienx
+        differencey = micro.accelerometer.get_y() - ancieny
         self.direction = (differencex, differencey)
-        ancienx = accelerometer.get_x()
-        ancieny = accelerometer.get_y()
+        ancienx = micro.accelerometer.get_x()
+        ancieny = micro.accelerometer.get_y()
 
-    def grandit(self):
-        if self.avance() ==
+    def colisions(self):
+        if self.corps.dernier.valeur[0] > 4 or self.corps.dernier.valeur[0] < 0:
+            return False
+        elif self.corps.dernier.valeur[1] > 4 or self.corps.dernier.valeur[1] < 0:
+            return False
+        elif self.corps.appartient_spe(self.dernier.valeur):
+            return False
+        else:
+            return True
+                
 
 
